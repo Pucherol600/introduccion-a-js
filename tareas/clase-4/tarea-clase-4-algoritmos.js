6aee378400fd313ec4aa0d1f36f0068eee0c0829
Pucherol600 <55899726+Pucherol600@users.noreply.github.com>
2020-08-27 09:29:28 -0300

    se agrega la solucion al ejercicio 14

--- tareas/clase-4/tarea-clase-4-algoritmos.js
+++ tareas/clase-4/tarea-clase-4-algoritmos.js
@@ -215,15 +215,14 @@ function fibo(){
 
 Desafío de programación #15: Crear una función que encuentre el número n de Fibonacci usando recursión
 */
-function findFibonacci(n)
-{
+function findFibonacci(n){
     if (n == 0)
         return 0;   
     if (n == 1)
         return 1;
     return findFibonacci(n - 1) + findFibonacci(n - 2);
 }
-console.log(findFibonacci(3));
+
 
 
 /*
